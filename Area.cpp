31869647048580725e70776ee741db2777adf52a
DarkM1nd <125229600+VZNURUIM@users.noreply.github.com>
2023-11-26 15:03:16 -0300

    Add files via upload

---
+++ Area.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main() {
+	double ray;
+	
+	cout << "Write the ray: ";
+	cin >> ray;
+	
+	double area_formula = (3.14159265359 * (pow(ray, 2.0)));
+
+	cout << "\n\nArea: " << area_formula << "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n";
+
+	return 0;
+}
