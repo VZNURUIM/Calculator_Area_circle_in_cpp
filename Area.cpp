aa925773f9f92bc779cb254646551b8a36823d1d
DarkM1nd <125229600+VZNURUIM@users.noreply.github.com>
2023-11-26 15:14:03 -0300

    Update Area.cpp

--- Area.cpp
+++ Area.cpp
@@ -1,17 +1,22 @@
 #include <iostream>
 #include <math.h>
+#include <string>
 
 using namespace std;
 
 int main() {
+	string response;
 	double ray;
+
+	cout << "Write: \ncm2\nm2\nkm2\nmm2\nResponse: ";
+	cin >> response;
 	
-	cout << "Write the ray: ";
+	cout << "\nWrite the ray: ";
 	cin >> ray;
 	
 	double area_formula = (3.14159265359 * (pow(ray, 2.0)));
 
-	cout << "\n\nArea: " << area_formula << "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n";
+	cout << "\n\nArea: " << area_formula << " " << response << "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n";
 
 	return 0;
 }
